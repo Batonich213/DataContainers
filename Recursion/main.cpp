8be46200bf151f0ef4beec6923042ed3d2b403e3
Никита Ботов <botov164@gmail.com>
2023-04-11 20:40:03 +0300

    Recursion

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void elevator(int floor)
 {
-	if (floor == 0)
+	if (floor <= 0)
 	{
 		cout << "This is basement";
 		return;
@@ -13,14 +13,24 @@ void elevator(int floor)
 	elevator(floor - 1);
 }
 
-
+int factorial(int n)
+{
+	if (n < 0)
+		return 0; 
+	if (n == 0) 
+		return 1; 
+	else 
+		return n * factorial(n - 1);
+}
 
 void main()
 {
 	//cout << "Hello Recursion";
 	//main();
 	int n;
-	cout << "Enter floor number: "; cin >> n;
-	elevator(n);
+	cout << "Enter nubmer";
+	 cin >> n;
+	factorial(n);
+	cout << "Factorial for " << n << " is " << factorial(n) << endl;
 }
 
