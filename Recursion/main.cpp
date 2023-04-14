c44fdc9196a3cc7f245e02226c6dcb76d2fbb95f
Никита Ботов <botov164@gmail.com>
2023-04-14 19:18:41 +0300

    Fibonacci VER 6 DONE(I HATE MATH)

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -18,10 +18,10 @@ void elevator(int floor)
 int factorial(int n)
 {
 	if (n < 0)
-		return 0; 
-	if (n == 0) 
-		return 1; 
-	else 
+		return 0;
+	if (n == 0)
+		return 1;
+	else
 		return n * factorial(n - 1);
 }
 
@@ -36,10 +36,13 @@ double power(double a, int n)
 	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / power(a, -n);
 }
 
-double Fibonacci(int n)
+void Fibonacci(long long n, long long int a = 0, long long int b =1)
 {
-	if (n <= 2) return 1;
-	else if(n > 2) return (Fibonacci(n - 1) + Fibonacci(n - 2));
+	if (a + b >= n)return;
+	cout << a + b << "\t";
+	Fibonacci(n, b, a + b);
+	//if (n <= 2) return 1;
+	//else if (n > 2) return (Fibonacci(n - 1) + Fibonacci(n - 2));
 	//return n <= 2 ? 1 : n >=2 ? (Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
@@ -54,23 +57,20 @@ void main()
 	int limit;
 	//cout << "Enter number";
 	 //cin >> a;
-	 cout << "Enter power ";
-	 cin >> limit;
-	 //cout << a << " ^ " << n << " is " << power(a, n) << endl;
-	 
-	//factorial(n);
-	//cout << "Factorial for " << n << " is " << factorial(n) << endl;
+	cout << "Enter limit ";
+	cin >> limit;
+	//cout << a << " ^ " << n << " is " << power(a, n) << endl;
 
-	 //int t = 2;
-	 //int f = 4;
-	 //int l = 5;
-	 //cout << t + f * l << endl; // wow
+   //factorial(n);
+   //cout << "Factorial for " << n << " is " << factorial(n) << endl;
+
+	//int t = 2;
+	//int f = 4;
+	//int l = 5;
+	//cout << t + f * l << endl; // wow
 
 
-
-	 for (int i = 0; i <= limit; i++)
-	 {
-		 cout << Fibonacci(i) << " " << endl;
-	 }
+	Fibonacci(limit);
+	
 }
 
